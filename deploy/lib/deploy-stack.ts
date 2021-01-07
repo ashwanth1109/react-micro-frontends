60a8ad3e81d9f7b489eed9e887485a7642523896
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-08 00:06:54 +0530

    check: test github action

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,10 +1,24 @@
-import { App, Stack, StackProps } from "@aws-cdk/core";
+import { App, RemovalPolicy, Stack, StackProps } from "@aws-cdk/core";
 import { Bucket } from "@aws-cdk/aws-s3";
+// import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
 
 export class DeployStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, "react-mfe-w4544");
+    const bucketName = "react-mfe-w4544";
+    const destinationBucket = new Bucket(this, bucketName, {
+      bucketName,
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html",
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+
+    // new BucketDeployment(this, "DeployFEAssets", {
+    //   destinationBucket,
+    //   sources: [Source.asset("../container/dist")],
+    //   destinationKeyPrefix: "container",
+    // });
   }
 }
