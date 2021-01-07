6a62b9ae53a93ed147cc6d916620303ef31a13ae
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-07 23:46:10 +0530

    deploy: add s3 bucket

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,7 +1,10 @@
 import { App, Stack, StackProps } from "@aws-cdk/core";
+import { Bucket } from "@aws-cdk/aws-s3";
 
 export class DeployStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new Bucket(this, "react-mfe-w4544");
   }
 }
