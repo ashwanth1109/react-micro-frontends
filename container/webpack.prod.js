912d10017f372d0ed61a798c12eab0f1dc30eb2a
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-08 04:14:50 +0530

    container: load landing module on production

--- container/webpack.prod.js
+++ container/webpack.prod.js
@@ -1,5 +1,9 @@
 const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("./package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
 
 module.exports = () => {
   const prodConfig = {
@@ -8,6 +12,15 @@ module.exports = () => {
       publicPath: "/",
       filename: "[name].[hash].js",
     },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "container",
+        remotes: {
+          landing: `landing@${domain}/landing/remoteEntry.js`,
+        },
+        shared: packageJson.dependencies,
+      }),
+    ],
   };
 
   return merge(commonConfig, prodConfig);
