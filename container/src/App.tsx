d7968f4e5118c3fcb30f57c15cbfd6516b9093f1
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-08 14:22:34 +0530

    container: add react routing for landing and auth

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,19 +1,28 @@
 import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const LandingLazy = lazy(() => import("./modules/Landing"));
 const AuthLazy = lazy(() => import("./modules/Auth"));
 
 const App = () => {
   return (
-    <div>
-      <h1>Container App</h1>
+    <Router>
       <div>
-        <Suspense fallback={<div>Loading . . .</div>}>
-          <LandingLazy />
-          <AuthLazy />
-        </Suspense>
+        <h1>Container App</h1>
+        <div>
+          <Suspense fallback={<div>Loading . . .</div>}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy />
+              </Route>
+              <Route path="/">
+                <LandingLazy />
+              </Route>
+            </Switch>
+          </Suspense>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
--- container/webpack.dev.js
+++ container/webpack.dev.js
@@ -13,7 +13,7 @@ module.exports = () => {
     devServer: {
       port: 8080,
       historyApiFallback: {
-        index: "index.html",
+        index: "/",
       },
     },
     plugins: [
